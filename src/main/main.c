4b73ac39d1e9863360603a1de958ba24b3b6106d
ari-feiglin <ari.feiglin@gmail.com>
2020-10-27 10:25:56 +0200

    removed garbage

--- src/main/main.c
+++ src/main/main.c
@@ -8,26 +8,8 @@ int magic_len = 0;
 int extension_len = 0;
 
 int main(int argc, char ** argv){
-    char * database_name = "DaFAQ Database";
-    
-    int error_check = 0;
-    record_field ** record = NULL;
-    int fd = 0;
-    int sort_fd = 0;
-    int record_numbers = 0;
-    int num_of_fields = 0;
-    int i = 0;
-    int j = 1;
-    char target[] = "zzzz";
-    int itarget = 20;
-    bool * map = NULL;
-
     magic_len = strnlen(magic, STRING_LEN);
     extension_len = strnlen(extension, STRING_LEN);
-
-    if(2 == argc){
-        database_name = argv[1];
-    }
 
     print_menu();
 }
